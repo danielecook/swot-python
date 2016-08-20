b14802a512f794abcb3dee5146f4a9766ee14349
XinyanXu <xynxu@ucdavis.edu>
2016-08-19 21:59:16 -0700

    update domain data and add school_name function

--- swot.py
+++ swot.py
@@ -3,6 +3,9 @@ import re
 import tldextract
 from os.path import join, dirname, exists
 import six
+import codecs
+import unidecode
+#import unicodedata
 
 
 BLACKLIST = frozenset([
@@ -10,7 +13,8 @@ BLACKLIST = frozenset([
     'america.edu',
     'californiacolleges.edu',
     'australia.edu',
-    'cet.edu'
+    'cet.edu',
+    'folger.edu'
 ])
 
 ACADEMIC_TLDS = frozenset([
@@ -54,6 +58,49 @@ ACADEMIC_TLDS = frozenset([
     'ac.za',
     'ac.zm',
     'ac.zw',
+    'cc.al.us',
+    'cc.ar.us',
+    'cc.az.us',
+    'cc.ca.us',
+    'cc.co.us',
+    'cc.fl.us',
+    'cc.ga.us',
+    'cc.hi.us',
+    'cc.ia.us',
+    'cc.id.us',
+    'cc.il.us',
+    'cc.in.us',
+    'cc.ks.us',
+    'cc.ky.us',
+    'cc.la.us',
+    'cc.md.us',
+    'cc.me.us',
+    'cc.mi.us',
+    'cc.mn.us',
+    'cc.mo.us',
+    'cc.ms.us',
+    'cc.mt.us',
+    'cc.nc.us',
+    'cc.nd.us',
+    'cc.ne.us',
+    'cc.nj.us',
+    'cc.nm.us',
+    'cc.nv.us',
+    'cc.ny.us',
+    'cc.oh.us',
+    'cc.ok.us',
+    'cc.or.us',
+    'cc.pa.us',
+    'cc.ri.us',
+    'cc.sc.us',
+    'cc.sd.us',
+    'cc.tx.us',
+    'cc.va.us',
+    'cc.vi.us',
+    'cc.wa.us',
+    'cc.wi.us',
+    'cc.wv.us',
+    'cc.wy.us',
     'ed.ao',
     'ed.cr',
     'ed.jp',
@@ -175,6 +222,43 @@ ACADEMIC_TLDS = frozenset([
     'sch.uk',
     'school.nz',
     'school.za',
+    'tec.ar.us',
+    'tec.az.us',
+    'tec.co.us',
+    'tec.fl.us',
+    'tec.ga.us',
+    'tec.ia.us',
+    'tec.id.us',
+    'tec.il.us',
+    'tec.in.us',
+    'tec.ks.us',
+    'tec.ky.us',
+    'tec.la.us',
+    'tec.ma.us',
+    'tec.md.us',
+    'tec.me.us',
+    'tec.mi.us',
+    'tec.mn.us',
+    'tec.mo.us',
+    'tec.ms.us',
+    'tec.mt.us',
+    'tec.nc.us',
+    'tec.nd.us',
+    'tec.nh.us',
+    'tec.nm.us',
+    'tec.nv.us',
+    'tec.ny.us',
+    'tec.oh.us',
+    'tec.ok.us',
+    'tec.pa.us',
+    'tec.sc.us',
+    'tec.sd.us',
+    'tec.tx.us',
+    'tec.ut.us',
+    'tec.vi.us',
+    'tec.wa.us',
+    'tec.wi.us',
+    'tec.wv.us',
     'vic.edu.au'
 ])
 
@@ -213,11 +297,42 @@ class Swot(object):
         if exists(join(dirname(__file__), path)):
             return True
 
-        if exists(join(sys.prefix, 'swot_data', path)):
-            return True
-
         return False
 
+    @classmethod
+    def school_name(cls, domain_str):
+        if not domain_str or not isinstance(domain_str, six.string_types):
+            return 'domain error'
+
+        domain_str = domain_str.strip().lower()
+        domain = tldextract.extract(domain_str)
+        if not domain.registered_domain:
+            return 'domain error'
+
+        if [b for b in BLACKLIST if re.search(r'(\A|\.){0}'.format(re.escape(b)), domain_str)]:
+            return 'unknown'
+
+        if (domain.tld in ACADEMIC_TLDS) | (Swot.__is_academic_domain(domain)):
+            path = '{0}.txt'.format(
+                join(
+                    'data/lib/domains',
+                    join(*reversed(domain.registered_domain.split('.')))
+                )
+            )
+            abs_path = join(dirname(__file__), path)
+            if(exists(abs_path)):
+                f = codecs.open(abs_path, "r", encoding = "utf-8")
+                content = f.read()
+                f.close
+                return re.sub(r'\n', '', unidecode.unidecode(content))
+            return 'unknown'
+
+        return 'unknown'
 
 if __name__ == '__main__':
+    #print(Swot.is_academic(sys.argv[1]))
+    #print(Swot.is_academic("ucdavis.edu"))
+    #print(Swot.is_academic("lbl.gov"))
+    #print(Swot.school_name("lbl.gov"))
+    print(Swot.school_name(sys.argv[1]))
     print(Swot.is_academic(sys.argv[1]))
